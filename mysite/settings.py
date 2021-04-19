abe4571941e6da75009ad0d46f04ba0796c37a9f
Anton de Winter <adewinter@gmail.com>
2021-04-19 10:17:03 -0700

    Most of the scaffold work done hopefully

--- mysite/settings.py
+++ mysite/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'timezones.apps.TimezonesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- timezones/models.py
+++ timezones/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Country(models.Model):
+    code = models.CharField(max_length=5)
+    name = models.CharField(max_length=255)
+    primary_language = models.CharField(max_length=255)
+
+class Cheers(models.Model):
+    country = models.ForeignKey(Country, on_delete=models.CASCADE)
+    raw = models.CharField(max_length=255)
+    pronunciation = models.CharField(max_length=255)
+
