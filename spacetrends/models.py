c06fd607c7622e1af84663805aff15910220f9a3
Anton de Winter <adewinter@gmail.com>
2021-05-24 20:37:07 +0000

    working...

--- spacetrends/models.py
+++ spacetrends/models.py
@@ -5,41 +5,63 @@ class Orbit(models.Model):
     code = models.CharField(max_length=255)
     notes = models.TextField()
 
+    def __str__(self):
+        return f"{self.name} ({self.code})"
+
 class Site(models.Model):
     name = models.CharField(max_length=255)
     code = models.CharField(max_length=255)
     notes = models.TextField()
 
+    def __str__(self):
+        return f"{self.name} ({self.code})"
+
+
+class SiteSummary(models.Model):
+    '''
+        Gives a launch site's stats for a given year
+    '''
+    year = models.IntegerField()
+    site = models.ForeignKey(Site)
+    launch_attempts = models.IntegerField(default=0)
+    launch_fails = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.site.name} Site Summary ({self.year})"
+
 
 class Vehicle(models.Model):
     '''
-    Vehicle            Overall           By Orbit Type
-                   Launches      Earth-Orbit Earth-Escape
-                  (Failures)      LEO   >LEO  Deep Space
+        Launch Vehicle
     '''
     name = models.CharField(max_length=255)
-
     notes = models.TextField()
 
-class VehiclePerformanceData(models.Model):
+    def __str__(self):
+        return f"{self.name}"
+
+class VehiclePerformance(models.Model):
     '''
         Gives vehicle performance/reliability stats from first ever attempt up to and including published or end year
-                                   Lewis 
-                Successes  Point AdjWald   Consc. Last    Dates 
-Vehicle         /Attempts  Est*  95%CI*    Succes Fail 
     '''
-    inital_year = models.IntegerField()
-    end_year = models.IntegerField()
-    published_year = models.IntegerField()
+    vehicle = models.ForeignKey(Vehicle)
 
-    launch_attempts = models.IntegerField()
-    launch_successes = models.IntegerField()
-    launch_consecutive_successes = models.IntegerField()
+    inital_year = models.IntegerField(default=0)
+    end_year = models.IntegerField(blank=True)
+    published_year = models.IntegerField(default=0)
+
+    launch_attempts = models.IntegerField(default=0)
+    launch_successes = models.IntegerField(default=0)
+    launch_consecutive_successes = models.IntegerField(default=0)
 
     last_failure = models.DateField()
-    
 
-    reliability_estimate_lewis_point = 
+    reliability_estimate_lewis_point = models.FloatField(default=0.00)
+    reliability_estimate_adjusted_wald_ci_start = models.FloatField(default=0.00)
+    reliability_estimate_adjusted_wald_ci_end = models.FloatField(default=0.00)
+
+    def __str__(self):
+        return f"{self.vehicle.name} - Performance Statistics {self.initial_year} - {self.end_year} (published: {self.published_year})"
 
 
 class VehicleFlightSummary(models.Model):
@@ -49,17 +71,20 @@ class VehicleFlightSummary(models.Model):
     year = models.IntegerField()
     vehicle = models.ForeignKey(Vehicle)
 
-    launch_attempts = models.IntegerField()
-    launch_fails = models.IntegerField()
+    launch_attempts = models.IntegerField(default=0)
+    launch_fails = models.IntegerField(default=0)
 
-    leo_attempts = models.IntegerField()
-    leo_fails = models.IntegerField()
+    leo_attempts = models.IntegerField(default=0)
+    leo_fails = models.IntegerField(default=0)
 
-    beyond_leo_attempts = models.IntegerField()
-    beyond_leo_fails = models.IntegerField()
+    beyond_leo_attempts = models.IntegerField(default=0)
+    beyond_leo_fails = models.IntegerField(default=0)
 
-    deep_space_attempts = models.IntegerField()
-    deep_space_fails = models.IntegerField()
+    deep_space_attempts = models.IntegerField(default=0)
+    deep_space_fails = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.vehicle.name} - Attempts / Fails: {self.launch_attempts} / {self.launch_fails}"
 
 
 # DATE     VEHICLE           ID      PAYLOAD                 MASS(t) SITE*     ORBIT** NOTE 
