6192fb040822727faca655ed0b065e5d6e0ccb4d
Anton de Winter <adewinter@gmail.com>
2021-05-24 19:26:32 +0000

    testing github codespaces

--- spacetrends/apps.py
+++ spacetrends/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class TimezonesConfig(AppConfig):
+class SpacetrendsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'timezones'
+    name = 'spacetrends'
