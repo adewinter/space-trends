8e88e26f10255a0247980b3c8b77d23876ceeb36
Anton de Winter <adewinter@gmail.com>
2021-05-24 20:51:16 +0000

    roughly made all the models I want for now haven't tested yet though

--- spacetrends/models.py
+++ spacetrends/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Orbit(models.Model):
     name = models.CharField(max_length=255)
     code = models.CharField(max_length=255)
-    notes = models.TextField()
+    notes = models.TextField(blank=True)
 
     def __str__(self):
         return f"{self.name} ({self.code})"
@@ -11,7 +11,7 @@ class Orbit(models.Model):
 class Site(models.Model):
     name = models.CharField(max_length=255)
     code = models.CharField(max_length=255)
-    notes = models.TextField()
+    notes = models.TextField(blank=True)
 
     def __str__(self):
         return f"{self.name} ({self.code})"
@@ -22,7 +22,7 @@ class SiteSummary(models.Model):
         Gives a launch site's stats for a given year
     '''
     year = models.IntegerField()
-    site = models.ForeignKey(Site)
+    site = models.ForeignKey(Site, on_delete=models.CASCADE)
     launch_attempts = models.IntegerField(default=0)
     launch_fails = models.IntegerField(default=0)
 
@@ -35,7 +35,7 @@ class Vehicle(models.Model):
         Launch Vehicle
     '''
     name = models.CharField(max_length=255)
-    notes = models.TextField()
+    notes = models.TextField(blank=True)
 
     def __str__(self):
         return f"{self.name}"
@@ -44,17 +44,17 @@ class VehiclePerformance(models.Model):
     '''
         Gives vehicle performance/reliability stats from first ever attempt up to and including published or end year
     '''
-    vehicle = models.ForeignKey(Vehicle)
+    vehicle = models.ForeignKey(Vehicle, on_delete=models.CASCADE)
 
-    inital_year = models.IntegerField(default=0)
+    inital_year = models.IntegerField()
     end_year = models.IntegerField(blank=True)
-    published_year = models.IntegerField(default=0)
+    published_year = models.IntegerField()
 
-    launch_attempts = models.IntegerField(default=0)
+    launch_attempts = models.IntegerField()
     launch_successes = models.IntegerField(default=0)
     launch_consecutive_successes = models.IntegerField(default=0)
 
-    last_failure = models.DateField()
+    last_failure = models.DateField(blank=True)
 
     reliability_estimate_lewis_point = models.FloatField(default=0.00)
     reliability_estimate_adjusted_wald_ci_start = models.FloatField(default=0.00)
@@ -69,7 +69,7 @@ class VehicleFlightSummary(models.Model):
         Gives the vehicle flight summary over the course of a given year
     '''
     year = models.IntegerField()
-    vehicle = models.ForeignKey(Vehicle)
+    vehicle = models.ForeignKey(Vehicle, on_delete=models.CASCADE)
 
     launch_attempts = models.IntegerField(default=0)
     launch_fails = models.IntegerField(default=0)
@@ -92,13 +92,15 @@ class VehicleFlightSummary(models.Model):
 # Create your models here.
 class Launch(models.Model):
     launch_date = models.DateField()
-    vehicle = 
-    name = models.CharField(max_length=255)
-    primary_language = models.CharField(max_length=255)
+    vehicle = models.ForeignKey(Vehicle, on_delete=models.CASCADE)
+    launch_id = models.CharField(max_length=255)
+    payload = models.CharField(max_length=255)
+    mass = models.FloatField(default=0, help_text="Mass in tons")
+    site = models.ForeignKey(Site, on_delete=models.CASCADE)
+    site_pad_code = models.CharField(blank=True, help_text="Optional launch site pad code", max_length=255)
+    orbit = models.ForeignKey(Orbit, on_delete=models.CASCADE)
+
     notes = models.TextField()
 
-class Cheers(models.Model):
-    country = models.ForeignKey(Country, on_delete=models.CASCADE)
-    raw = models.CharField(max_length=255)
-    pronunciation = models.CharField(max_length=255)
-
+    def __str__(self):
+        return f"Launch [{self.launch_date}] - {self.vehicle.name} - {self.orbit.code}"
