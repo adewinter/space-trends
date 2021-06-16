02c11b9164f1793eafabc663437c0344c1dc31b7
Anton de Winter <adewinter@gmail.com>
2021-06-16 14:06:19 -0700

    Refactored command into a more object oriented style instead of throwing a bunch of global state around.  There is definitely some jank due to the fact that I need 'stdout' and 'Command.style' functions to print things to the terminal, but it's still worth it because I can now easily determine which file is being parsed at any time (in order to write any fixes I might manually make during parsing back to that same file).

--- spacetrends/management/commands/parselaunchstats.py
+++ spacetrends/management/commands/parselaunchstats.py
@@ -5,38 +5,33 @@ from os import scandir
 
 from django.core.management.base import BaseCommand, CommandError
 
+import pyperclip
+
 from spacetrends.models import Vehicle
 
-class Command(BaseCommand):
-    help = 'Parses the LAUNCH STATS txt data files located in the initial_data/ folder'
-
-    IS_OLD_STATS_STYLE = False
-    IS_DEBUG_MODE = False #Set to `True` when passed in --verbosity flag is greater than 1
-    CURRENT_YEAR = 0000 #oh boy some sweet global state
-
-    def add_arguments(self, parser):
-        # #positional arg
-        # parser.add_argument('poll_ids', nargs='+', type=int)
-        # Named (optional) arguments
-        parser.add_argument(
-            '--file', '-f',
-            action='store',
-            dest="statsfile",
-            help='Specify a specific file instead of parsing all the files in the spacetrends/initial_data/ folder.',
-        )
+class launch_stats_parser():
+    def __init__(self, stdout, style, filepath, filename, is_debug_mode, is_old_stats_style):
+        self.stdout = stdout
+        self.style = style
+        self.filepath = filepath
+        self.filename = filename
+        self.year = 0000
+        self.file_should_be_modified = False #if we perform a line fix in the middle of parsing, we can now persist the fix
+        self.is_debug_mode = is_debug_mode #Set to `True` when passed in --verbosity flag is greater than 1
+        self.is_old_stats_style = is_old_stats_style
 
     def pprint(self, msg):
         self.stdout.write(self.style.SUCCESS(msg))
 
     def debug_print(self, msg):
-        if self.IS_DEBUG_MODE:
+        if self.is_debug_mode:
             self.pprint(msg)
 
     def debug_pprint(self, msg):
         self.debug_print(pprint.pformat(msg))
 
     def get_launch_log_lines(self, lines):
-        match_string = 'LAUNCH LOG' if self.IS_OLD_STATS_STYLE else 'SPACE LAUNCH LOG'
+        match_string = 'LAUNCH LOG' if self.is_old_stats_style else 'SPACE LAUNCH LOG'
         self.debug_print(f"Using match string: {match_string}")
 
         last_match_index = -1
@@ -46,8 +41,10 @@ class Command(BaseCommand):
         self.debug_print(f"Found match string at index: {last_match_index}")
         lines = lines[last_match_index:]
         data_start_match_string = '--------------------------------'
+        data_start_raw_lines_index = last_match_index # used to track line numbers from the raw file
         for _, line in list(enumerate(lines)): #make a copy of the list in order to pop() without weird behavior
             lines.pop(0) #delete the line until we reach the match
+            data_start_raw_lines_index += 1
             if data_start_match_string in line:
                 break #we've already popped the current (and matching) line so the _next_ line should be the first data entry we're actually after
         
@@ -59,9 +56,12 @@ class Command(BaseCommand):
                 data_end_match_index = index
                 break; #stop at the first match
         
-        result = lines[:data_end_match_index]
-        self.pprint(f"Found {len(result)} launch log entries!")
+        data_end_raw_lines_index = data_start_raw_lines_index + data_end_match_index
+        result_lines = lines[:data_end_match_index]
 
+        self.pprint(f"Found {len(result_lines)} launch log entries! RAW LINE NUMBER START: {data_start_raw_lines_index}, END: {data_end_raw_lines_index}")
+
+        result = dict(zip(range(data_start_raw_lines_index, data_end_raw_lines_index), result_lines))
         return result
 
     def parse_launch_log_line_into_list_symbols(self, line):
@@ -133,13 +133,20 @@ class Command(BaseCommand):
     def create_launch_in_db(self, launch):
         pass
 
+    def update_file_with_fixes(lines):
+        self.pprint("WRITE ME!!!!! update_file_with_fixes()")
 
     def populate_db_from_launch_log(self, lines):
         # self.debug_print(pprint.pformat(lines))
-        for index, line in enumerate(lines):
+        for index, line in lines.items():
             entry = self.parse_launch_log_line_into_list_symbols(line)
-            if len(entry) < 7:
-                raise ValueError(f"Bad parse for (#{index})line: {line}, {entry}")
+            while len(entry) < 7:
+                pyperclip.copy(line.strip('\n'))
+                fixed_line = input(f"Bad parse for line [{index}]:\n{line}\n.  Please provide fixed version:\n")
+                lines[index] = fixed_line
+                self.CURRENT_FILE_MODIFIED = True
+                entry = self.parse_launch_log_line_into_list_symbols(fixed_line)
+                
             self.debug_pprint(entry)
             launch_date = self.parse_date_from_raw_date(entry[0])
             site_name, site_code = self.parse_launch_site_name_and_code_from_raw(entry[5])
@@ -160,16 +167,22 @@ class Command(BaseCommand):
 
             self.create_launch_in_db(launch)
 
+        if self.CURRENT_FILE_MODIFIED:
+            self.pprint(f"Detected some lines in file have been fixed. Writing fixes back to file.")
+            self.pprint(pprint.pformat(lines))
+            self.update_file_with_fixes(lines)
 
 
-    def parse_stats_file(self, statsfilepath, filename):
-        with open(statsfilepath) as statsfile:
+
+    def parse_stats_file(self):
+        self.CURRENT_FILE_MODIFIED = False
+        with open(self.filepath) as statsfile:
             self.debug_print(f"reading file: {statsfile}")
             statsfile_lines = statsfile.readlines()
 
         # Set the 'year' this file was made in
         year_regex = r'\d{4}'
-        match = re.search(year_regex, filename)
+        match = re.search(year_regex, self.filename)
         if match:
             self.CURRENT_YEAR = int(match[0])
 
@@ -178,6 +191,32 @@ class Command(BaseCommand):
         self.debug_print("Populating DB with launch log entries...")
         self.populate_db_from_launch_log(launch_log_lines)
 
+
+
+class Command(BaseCommand):
+    help = 'Parses the LAUNCH STATS txt data files located in the initial_data/ folder'
+    IS_DEBUG_MODE = False #Set to `True` when passed in --verbosity flag is greater than 1
+
+    def add_arguments(self, parser):
+        # #positional arg
+        # parser.add_argument('poll_ids', nargs='+', type=int)
+        # Named (optional) arguments
+        parser.add_argument(
+            '--file', '-f',
+            action='store',
+            dest="statsfile",
+            help='Specify a specific file instead of parsing all the files in the spacetrends/initial_data/ folder.',
+        )
+
+    def pprint(self, msg):
+        self.stdout.write(self.style.SUCCESS(msg))
+
+    def debug_print(self, msg):
+        if self.IS_DEBUG_MODE:
+            self.pprint(msg)
+
+    def debug_pprint(self, msg):
+        self.debug_print(pprint.pformat(msg))
 
     def handle(self, *args, **options):
         if options['verbosity'] > 1:
@@ -189,10 +228,14 @@ class Command(BaseCommand):
             filename = options['statsfile']
             if ('2001' in filename) or ('2002' in filename):
                 self.debug_print(f"Using old stats style parsing for {filename}")
-                self.IS_OLD_STATS_STYLE = True
+                is_old_stats_style = True
             else:
-                self.IS_OLD_STATS_STYLE = False
-            self.parse_stats_file(options['statsfile'], filename)
+                is_old_stats_style = False
+
+        #     def __init__(self, stdout, filepath, filename, is_debug_mode, is_old_stats_style):
+
+            parser = launch_stats_parser(self.stdout, self.style, options['statsfile'], filename, self.IS_DEBUG_MODE, is_old_stats_style)
+            parser.parse_stats_file()
         else:
             with scandir('spacetrends/initial_data') as entries:
                 for entry in entries:
@@ -200,19 +243,8 @@ class Command(BaseCommand):
                         self.pprint(f"File name: {entry.name}")
                         if ('2001' in entry.name) or ('2002' in entry.name):
                             self.debug_print(f"Using old stats style parsing for {entry.name}")
-                            self.IS_OLD_STATS_STYLE = True
+                            is_old_stats_style = True
                         else:
-                            self.IS_OLD_STATS_STYLE = False
-                        self.parse_stats_file(entry.path, entry.name)
-
-
-        # for poll_id in options['poll_ids']:
-        #     try:
-        #         poll = Poll.objects.get(pk=poll_id)
-        #     except Poll.DoesNotExist:
-        #         raise CommandError('Poll "%s" does not exist' % poll_id)
-
-        #     poll.opened = False
-        #     poll.save()
-
-        #     self.stdout.write(self.style.SUCCESS('Successfully closed poll "%s"' % poll_id))
+                            is_old_stats_style = False
+                        parser = launch_stats_parser(self.stdout, self.style, entry.path, entry.name, self.IS_DEBUG_MODE, is_old_stats_style)
+                        parser.parse_stats_file()
