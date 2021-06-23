f42f51471e0e52c1162aaab0d6912265659a2019
Anton de Winter <adewinter@gmail.com>
2021-06-23 16:08:38 -0700

    Admin panel for Launch and Orbit models

--- spacetrends/admin.py
+++ spacetrends/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from spacetrends.models import Launch, Orbit
 
-# Register your models here.
+
+class LaunchAdmin(admin.ModelAdmin):
+	date_hierarchy = 'launch_date'
+	list_display = ('launch_date', 'vehicle', 'orbit', 'payload')
+
+admin.site.register(Launch, LaunchAdmin)
+
+class OrbitAdmin(admin.ModelAdmin):
+	list_display = ('name', 'code')
+
+admin.site.register(Orbit, OrbitAdmin)
